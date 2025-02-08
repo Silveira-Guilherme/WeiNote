62cbce6425bbd81ede091d3b5213abac8cdd3653
Guilherme Silveira <133682570+Silveira-Guilherme@users.noreply.github.com>
2025-02-08 00:46:36 +0000

    UI Updates (#59)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gymdo/init.dart';
+
 import 'package:intl/date_symbol_data_local.dart';
 
-const Color primaryColor = Color.fromARGB(255, 0, 0, 0); // Example primary color
-const Color secondaryColor = Color.fromARGB(255, 255, 255, 255); // Example secondary color
+const Color primaryColor =
+    Color.fromARGB(255, 0, 0, 0); // Example primary color
+const Color secondaryColor =
+    Color.fromARGB(255, 255, 255, 255); // Example secondary color
 //const Color accentColor1 = Color.fromARGB(255, 0, 0, 0);
 const Color accentColor1 = Color.fromARGB(255, 48, 48, 48);
 const Color accentColor2 = Colors.red; // Example accent color 2
